8a17aa2042f48dfdeb26a5ece6fcb516512a7a7e
valvide <semeongizachew69@gmail.com>
2022-12-08 10:41:00 -0800

    Login.js moved

---
+++ src/components/Login.js
@@ -0,0 +1,75 @@
+import { Button } from "@mui/material";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React from "react";
+import styled from "styled-components";
+import { auth, provider } from "../firebase";
+
+function Login() {
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  return (
+    <LoginContainer>
+      <LoginInnerContainer>
+        <img
+          src="https://www.marinfc.org/wp-content/uploads/sites/482/2020/03/Slack_RGB.png"
+          alt=""
+        />
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </LoginInnerContainer>
+    </LoginContainer>
+  );
+}
+
+export default Login;
+
+const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+  > img {
+    object-fit: contain;
+    height: 200px;
+
+    margin-bottom: 10px;
+  }
+
+  > button {
+    margin-top: 20px;
+    text-transform: inherit !important;
+    background-color: #0a8d48 !important;
+    color: white;
+    max-width: 220px !important;
+  }
+`;
