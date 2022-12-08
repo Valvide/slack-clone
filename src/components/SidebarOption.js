ecd3670b713678eacf6c21a6fc03276a3a642cb3
valvide <semeongizachew69@gmail.com>
2022-12-08 10:42:52 -0800

    Finsihing touch

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useDispatch } from "react-redux";
+import { enterRoom, setMessages } from "../features/appSlice";
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
+  const addChannel = async () => {
+    console.log("prop test", addChannelOption);
+    const channelName = prompt("Please enter the channel name");
+    if (channelName) {
+      const docRef = await addDoc(collection(db, "rooms"), {
+        name: channelName,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    }
+  };
+
+  const selectChannel = () => {
+    if (id) {
+      dispatch(enterRoom({ roomId: id }));
+    }
+  };
+
+  return (
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span>
+          {title}
+        </SidebarOptionChannel>
+      )}
+    </SidebarOptionContainer>
+  );
+}
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+  > h3 {
+    font-weight: 500;
+  }
+  > h3 > span {
+    padding: 15px;
+  }
+`;
+
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
